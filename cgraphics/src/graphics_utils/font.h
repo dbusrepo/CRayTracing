e17d023819ad49729b3edf8c1d8a598313660299
dbusato <davidedbusato@gmail.com>
2020-11-21 12:01:39 +0100

    Restructure proj

--- cgraphics/src/graphics_utils/font.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "screen_info.h"
-
-void font_init(screen_info_t *screen_info);
-
-void draw_text(screen_info_t *screen_info, uint32_t xsize, uint32_t ysize, uint32_t x, uint32_t y, const char *text);
-
-void set_text_color(screen_info_t *screen_info, uint32_t red, uint32_t green, uint32_t blue);
